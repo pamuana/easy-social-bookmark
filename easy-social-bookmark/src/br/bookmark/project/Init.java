b5f70e08a92afda8bfc7517638297a01e14a5257
geiser.gcc <geiser.gcc@74add99a-10a6-11de-b0d4-7fec019aa467>
2009-03-16 19:32:33 +0000

    session manager fixed

--- easy-social-bookmark/src/br/bookmark/project/Init.java
+++ easy-social-bookmark/src/br/bookmark/project/Init.java
@@ -10,23 +10,76 @@ import br.bookmark.models.UserDAO;
 
 public class Init {
 	public static Database database = null;
-    public static String urlCon;
-    public static UserDAO userDAO = null;
-    public static BookmarkDAO bookmarkDAO = null;
-    public static CommunityDAO communityDAO = null;
-    public static CommentDAO commentDAO = null;
-    public static MessageDAO messageDAO = null;
-    public static TagDAO tagDAO=null;
+    public static String urlCon="";
+    public static String prefixoTable="";
+    private UserDAO userDAO = null;
+    private BookmarkDAO bookmarkDAO = null;
+    private CommunityDAO communityDAO = null;
+    private CommentDAO commentDAO = null;
+    private MessageDAO messageDAO = null;
+    private TagDAO tagDAO=null;
     
     
-    public static void init(String pUrlCon) throws Exception{
+    public static void init(String pUrlCon,String pPrefixoTable) throws Exception{
     	urlCon = pUrlCon;
+    	prefixoTable = pPrefixoTable;
     	database = new Database("MySQL","com.mysql.jdbc.Driver", urlCon);
-    	userDAO = new UserDAO("",database);
-    	bookmarkDAO = new BookmarkDAO("",database);
-    	communityDAO = new CommunityDAO("",database);
-    	commentDAO = new CommentDAO("",database);
-    	messageDAO = new MessageDAO("",database);
-    	tagDAO = new TagDAO("",database);
     }
+    
+    public Init() throws Exception{
+    	this.setUserDAO(new UserDAO(prefixoTable,database));
+    	this.setBookmarkDAO(new BookmarkDAO(prefixoTable,database));
+    	this.setCommunityDAO(new CommunityDAO(prefixoTable,database));
+    	this.setCommentDAO(new CommentDAO(prefixoTable,database));
+    	this.setMessageDAO(new MessageDAO(prefixoTable,database));
+    	this.setTagDAO(new TagDAO(prefixoTable,database));
+    }
+
+	public void setUserDAO(UserDAO userDAO) {
+		this.userDAO = userDAO;
+	}
+
+	public UserDAO getUserDAO() {
+		return userDAO;
+	}
+
+	public void setBookmarkDAO(BookmarkDAO bookmarkDAO) {
+		this.bookmarkDAO = bookmarkDAO;
+	}
+
+	public BookmarkDAO getBookmarkDAO() {
+		return bookmarkDAO;
+	}
+
+	public void setCommunityDAO(CommunityDAO communityDAO) {
+		this.communityDAO = communityDAO;
+	}
+
+	public CommunityDAO getCommunityDAO() {
+		return communityDAO;
+	}
+
+	public void setCommentDAO(CommentDAO commentDAO) {
+		this.commentDAO = commentDAO;
+	}
+
+	public CommentDAO getCommentDAO() {
+		return commentDAO;
+	}
+
+	public void setMessageDAO(MessageDAO messageDAO) {
+		this.messageDAO = messageDAO;
+	}
+
+	public MessageDAO getMessageDAO() {
+		return messageDAO;
+	}
+
+	public void setTagDAO(TagDAO tagDAO) {
+		this.tagDAO = tagDAO;
+	}
+
+	public TagDAO getTagDAO() {
+		return tagDAO;
+	}
 }
